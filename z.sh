d97c4959f9c19911de8bb4980ec1dc9a9b0817f6
hsiang-cy <hsiang2000.ch@gmail.com>
2026-02-20 16:54:06 +0800

    初次佈署成功，也測試成功

---
+++ z.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 使用 shopt 包含隱藏檔案，並讓 * 匹配所有檔案
+shopt -s dotglob
+
+for name in *; do
+  # 排除目錄（cat 不能讀取目錄）、排除特定檔案、排除 . 和 ..
+  if [ -f "$name" ] && [ "$name" != "pnpm-lock.yaml" ] && [ "$name" != "z.sh" ]; then
+    printf "File: %s\n" "$name"
+    printf "=====================\n"
+    cat "$name"
+    printf "\n\n"
+  fi
+done
